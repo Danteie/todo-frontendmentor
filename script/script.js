84a1b1f33e14839f7ef9cc131e299732f053d48a
unknown <aleksandar.djuratovic2@gmail.com>
2022-04-01 14:14:14 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+
+
+function getInput() {
+    var mainInput = document.getElementById('html').value;
+    console.log(mainInput);
+    var list = document.getElementById('list');
+    var li = document.createElement("li");
+    var newLi = document.getElementById('main-input-div')
+    li.appendChild(newLi);
+    list.appendChild(li);
+}
+
+
+
